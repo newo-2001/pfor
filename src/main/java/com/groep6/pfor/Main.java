c2ab8724aa532ea4b6a5bfc1088453d22141bc5b
Owen Elderbroek <s1122238@student.hsleiden.nl>
2020-05-19 11:17:20 +0200

    Initialized maven project

---
+++ src/main/java/com/groep6/pfor/Main.java
@@ -0,0 +1,15 @@
+package com.groep6.pfor;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        System.out.println("Hello World!");
+    }
+}
