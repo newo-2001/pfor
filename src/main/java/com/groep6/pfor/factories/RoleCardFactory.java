38a493a10d0b7f2d5f1783efbc9f501417ce34a1
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-05-27 11:11:01 +0200

    Added 3 roles to roleCardFactory

--- src/main/java/com/groep6/pfor/factories/RoleCardFactory.java
+++ src/main/java/com/groep6/pfor/factories/RoleCardFactory.java
@@ -1,6 +1,9 @@
 package com.groep6.pfor.factories;
 
 import com.groep6.pfor.models.cards.RoleCard;
+import com.groep6.pfor.models.cards.actions.ConsulAction;
+import com.groep6.pfor.models.cards.actions.MagisterMilitumAction;
+import com.groep6.pfor.models.cards.actions.ReginaFoederataAction;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,18 +18,27 @@ public class RoleCardFactory {
     private List<RoleCard> roleCards = new ArrayList<>();
 
     private RoleCardFactory() {
+
+        RoleCard MAGISTER_MILITUM = new RoleCard("Magister Militum", new MagisterMilitumAction());
+        RoleCard CONSUL = new RoleCard("Consul", new ConsulAction());
+        RoleCard REGINA_FOEDERATA = new RoleCard("ReginaFoederata", new ReginaFoederataAction());
+
+        roleCards.add(MAGISTER_MILITUM);
+        roleCards.add(CONSUL);
+        roleCards.add(REGINA_FOEDERATA);
+
     }
 
     /**
      * Makes sure you always get the same RoleCardFactory instance
-     * @return instance of RoleCardFactory
+     * @return Instance of RoleCardFactory
      */
     public static RoleCardFactory getInstance() {
         return SINGLE_INSTANCE;
     }
 
     /**
-     * @return List<RoleCard>
+     * @return List of role cards
      */
     public List<RoleCard> getAllRoleCards() {
         return roleCards;
@@ -34,7 +46,7 @@ public class RoleCardFactory {
 
     /**
      * Pick a random role card
-     * @return RoleCard
+     * @return Random role card
      */
     public RoleCard pickRandomRoleCard() {
         Random randomizer = new Random();
@@ -43,7 +55,7 @@ public class RoleCardFactory {
     }
 
     /**
-     * @return int
+     * @return Size of all role cards
      */
     public int getRoleCardCount() {
         return roleCards.size();
