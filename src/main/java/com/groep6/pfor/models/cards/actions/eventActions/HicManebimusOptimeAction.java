153c4a7474f23c018a29b81608779a9e69fe81fb
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-05-27 17:02:47 +0200

    Create HicManebimusOptimeAction.

---
+++ src/main/java/com/groep6/pfor/models/cards/actions/eventActions/HicManebimusOptimeAction.java
@@ -0,0 +1,45 @@
+package com.groep6.pfor.models.cards.actions.eventActions;
+
+import com.groep6.pfor.models.cards.actions.IAction;
+
+/**
+ * Implements (the first) action on Hic Manebimus Optime event card
+ * @author Mitchell van Rijswijk
+ *
+ */
+public class HicManebimusOptimeAction implements IAction {
+
+	/**
+	 * 
+	 */
+	public void execute() {
+		/*
+		 * ViewController.switchView(pickCityView);
+		 * ViewController.getCurrentView().viewFortCities();
+		 * City city1 = <<player selection of a city>>;
+		 * City city2 = <<player selection of a city>>;
+		 * City city3 = <<player selection of a city>>;
+		 * city1.addLegions(2); city2.addLegions(2); city3.addLegions(2);
+		 * card.discard();
+		 */
+	}
+
+	/**
+	 * Gets the name of the event.
+	 * @return The name of the event.
+	 * 
+	 */
+	public String getName() {
+		return "Hic Manebimus Optime";
+	}
+
+	/**
+	 * Gets the action description.
+	 * @return The action description.
+	 * 
+	 */
+	public String getDescription() {
+		return "Kies maximaal 3 steden met een fort. Leg op elk van deze steden maximaal 2 legioenen.";
+	}
+
+}
