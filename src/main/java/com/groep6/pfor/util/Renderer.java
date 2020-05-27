c8fb3998e7bce1d6db2ffd81ae64871bd1b387fa
Owen Elderbroek <s1122238@student.hsleiden.nl>
2020-05-27 10:54:49 +0200

    Added Vector2f

--- src/main/java/com/groep6/pfor/util/Renderer.java
+++ src/main/java/com/groep6/pfor/util/Renderer.java
@@ -1,4 +1,49 @@
 package com.groep6.pfor.util;
 
+import javafx.scene.Group;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ * Represents a renderer that is able to render JavaFX nodes.
+ * Used as an abstraction layer over some otherwise obnoxious JavaFX code.
+ */
 public class Renderer {
+    /** The stage that this renderer will render on. */
+    private Stage stage;
+
+    /** The scene that will be rendered. */
+    private Scene scene;
+
+    /** The root object of the scenegraph. */
+    private Parent root;
+
+    /**
+     * Constructs a renderer that will render on the given stage.
+     * @param stage The stage to render on.
+     * @param dimensions The size of the window.
+     * @return A new renderer instance.
+     */
+    public Renderer(Stage stage, Vector2f dimensions) {
+        this.stage = stage;
+        scene = new Scene(new Group(), dimensions.x, dimensions.y);
+    }
+
+    /**
+     * Set the root node for the new scenegraph effectively overriding
+     * everything that is there and starting a scene from scratch.
+     * @param root The node to act as the root of the scenegraph.
+     */
+    public void setSceneGraph(Parent root) {
+        this.root = root;
+    }
+
+    /**
+     * Render the current scenegraph, doing so will update the screen
+     * and show the new changes to the user.
+     */
+    public void render() {
+
+    }
 }
--- src/main/java/com/groep6/pfor/util/Vector2f.java
+++ src/main/java/com/groep6/pfor/util/Vector2f.java
@@ -1,4 +1,203 @@
 package com.groep6.pfor.util;
 
+/**
+ * Represents a vector in 2d space. Has mathematical applications
+ * but is also useful to wrap two float together, mostly used for coordinates.
+ *
+ * @author Owen Elderbroek
+ */
 public class Vector2f {
+    /** THe x-component of the vector */
+    public float x;
+
+    /** The y-component of the vector */
+    public float y;
+
+    /**
+     * Initialises a new vector with the components (0, 0)
+     * @return A new Vector2f
+     */
+    public Vector2f() {
+        this(0, 0);
+    }
+
+    /**
+     * Initialises a new vector with the given components.
+     * @param x The x-component to initialize this vector with.
+     * @param y The y-component to initialize this vector with.
+     * @return A new Vector2f
+     */
+    public Vector2f(float x, float y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    /**
+     * Initialises a new vector with the given integers.
+     * @param x The x-component to initialize this vector with.
+     * @param y The y-component to initialize this vector with.
+     * @return A new Vector2f
+     */
+    public Vector2f(int x, int y) {
+        this((float) x, (float) y);
+    }
+
+    /**
+     * Creates a new vector from another one. Effectively cloning the given vector.
+     * @param base The vector to be cloned.
+     * @return The cloned vector.
+     */
+    public Vector2f(Vector2f base) {
+        this(base.x, base.y);
+    }
+
+    /**
+     * Add a scalar to both components of this vector.
+     * @param scalar The amount to be added.
+     * @return Itself for chaining.
+     */
+    public Vector2f add(float scalar) {
+        return add(new Vector2f(scalar, scalar));
+    }
+
+    /**
+     * Add another vector to this one.
+     * @param vec The vector to add to this vector
+     * @return Itself for chaining.
+     */
+    public Vector2f add(Vector2f vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+    /**
+     * Subtract a scalar from both components of this vector.
+     * @param scalar The amount to be subtracted.
+     * @return Itself for chaining.
+     */
+    public Vector2f sub(float scalar) {
+        return sub(new Vector2f(scalar, scalar));
+    }
+
+    /**
+     * Subtract a vector from this one.
+     * @param vec The vector to subtract from this vector
+     * @return Itself for chaining.
+     */
+    public Vector2f sub(Vector2f vec) {
+        this.x -= vec.x;
+        this.y -= vec.y;
+        return this;
+    }
+
+    /**
+     * Multiply both components of this vector by a scalar.
+     * @param scalar The amount to be multiplied by.
+     * @return Itself for chaining.
+     */
+    public Vector2f mul(float scalar) {
+        return mul(new Vector2f(scalar, scalar));
+    }
+
+    /**
+     * Multiply this vector by another one.
+     * @param vec The vector to be multiplied by.
+     * @return Itself for chaining.
+     */
+    public Vector2f mul(Vector2f vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+    /**
+     * Divide both components of this vector by a scalar.
+     * @param scalar The amount to be divided by.
+     * @return Itself for chaining.
+     */
+    public Vector2f div(float scalar) {
+        return div(new Vector2f(scalar, scalar));
+    }
+
+    /**
+     * Divide this vector by another one.
+     * @param vec The vector to be divided by.
+     * @return Itself for chaining.
+     */
+    public Vector2f div(Vector2f vec) {
+        this.x += vec.x;
+        this.y += vec.y;
+        return this;
+    }
+
+    /**
+     * Round all the components of the vector down to the closest integer.
+     * @return Itself for chaining.
+     */
+    public Vector2f floor() {
+        x = (float) Math.floor(x);
+        y = (float) Math.floor(y);
+        return this;
+    }
+
+    /**
+     * Round all the components of the vector up to the closest integer.
+     * @return Itself for chaining.
+     */
+    public Vector2f ceil() {
+        x = (float) Math.ceil(x);
+        y = (float) Math.ceil(y);
+        return this;
+    }
+
+    /**
+     * Calculates the length (or magnitude) of this vector
+     * using the pythagorean theorem.
+     * @return
+     */
+    public float length() {
+        return (float) Math.sqrt(x*x + y*y);
+    }
+
+    /**
+     * Normalizes the components of this vector to be between -1 and 1.
+     * Effectively reducing its length to 1.
+     * @return Itself for chaining.
+     */
+    public Vector2f normalize() {
+        this.x /= length();
+        this.y /= length();
+        return this;
+    }
+
+    /**
+     * Calculates the absolute version of this vector.
+     * Effectively making all its values positive.
+     * @return Itself for chaining.
+     */
+    public Vector2f abs() {
+        x = Math.abs(x);
+        y = Math.abs(y);
+        return this;
+    }
+
+    /**
+     * Compute the dot product of this vector with another.
+     * The dot product represents how different both vectors are.
+     * @param vec The other vector to calculate the dot product with.
+     * @return The dot product of the two vectors.
+     */
+    public float dot(Vector2f vec) {
+        return x * vec.x + y * vec.y;
+    }
+
+    /**
+     * Compute the angle between these two vectors.
+     * @param vec The other vector to calculate the angle with.
+     * @return The angle between the two vectors in radians.
+     */
+    public float angle(Vector2f vec) {
+        return (float) Math.acos(dot(vec) / (length() * vec.length()));
+    }
 }
