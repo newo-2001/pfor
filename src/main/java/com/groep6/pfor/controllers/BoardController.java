aa311ff7feb33caebb0ae9585c0266507e5ba2af
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-05-27 16:16:52 +0200

    Scene switching

--- src/main/java/com/groep6/pfor/controllers/BoardController.java
+++ src/main/java/com/groep6/pfor/controllers/BoardController.java
@@ -1,5 +1,23 @@
 package com.groep6.pfor.controllers;
 
-public class BoardController {
+import com.groep6.pfor.views.BoardView;
+import com.groep6.pfor.views.MenuView;
+
+public class BoardController extends Controller {
+
+    private static final BoardController INSTANCE = new BoardController();
+
+    private BoardController() {};
+
+    public static BoardController getInstance() { return INSTANCE; }
+
+    @Override
+    void registerObserver() {
+
+    }
+
+    public void goToMenu() {
+        ViewController.showView(new MenuView(ViewController.getPrimaryStage()));
+    }
 
 }
--- src/main/java/com/groep6/pfor/controllers/ViewController.java
+++ src/main/java/com/groep6/pfor/controllers/ViewController.java
@@ -1,15 +1,35 @@
 package com.groep6.pfor.controllers;
 
+import com.groep6.pfor.views.MenuView;
+import com.groep6.pfor.views.View;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class ViewController extends Controller {
+public class ViewController {
 
-    public ViewController(Stage primaryStage) {
+    private static final ViewController INSTANCE = new ViewController();
 
+    private Stage primaryStage;
+
+    private ViewController() {}
+
+    public static ViewController getInstance() { return INSTANCE; }
+
+    public static void setPrimaryStage(Stage primaryStage) {
+        ViewController viewController = ViewController.getInstance();
+        viewController.primaryStage = primaryStage;
     }
 
-    @Override
-    void registerObserver() {
+    public static void showView(View view) {
+        ViewController viewController = ViewController.getInstance();
+        Stage primaryStage = viewController.getPrimaryStage();
+        Scene scene = view.getScene();
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
+    public static Stage getPrimaryStage() {
+        ViewController viewController = ViewController.getInstance();
+        return viewController.primaryStage;
     }
 }
