750d322e839be61621494b64a19e9c0c722d46d9
nilse <nilse@DESKTOP-Q197SHS.domain_not_set.invalid>
2020-05-27 09:19:30 +0200

    Dit is een test om te kijken of ik kan pushen

--- src/main/java/com/groep6/pfor/Main.java
+++ src/main/java/com/groep6/pfor/Main.java
@@ -11,5 +11,6 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         System.out.println("Hello World!");
+        System.out.println("Test");
     }
 }
