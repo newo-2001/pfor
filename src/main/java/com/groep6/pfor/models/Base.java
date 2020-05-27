a39e600359f1fc0fc5fa5fda08b0ca7c11ed6aff
nilse <nilse@DESKTOP-Q197SHS.domain_not_set.invalid>
2020-05-27 15:38:52 +0200

    Added JavaDoc for Base

--- src/main/java/com/groep6/pfor/models/Base.java
+++ src/main/java/com/groep6/pfor/models/Base.java
@@ -1,5 +1,11 @@
 package com.groep6.pfor.models;
 
+/**
+ * Represents a barbarian base
+ *
+ * @author Nils van der Velden
+ */
+
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
@@ -7,27 +13,55 @@ import java.util.List;
 public class Base<T extends Piece> extends Tile {
 	private List<T> pieces = new ArrayList<T>();
 	
+    /**
+     * Initializes a new City with the given components.
+     * @param position The Vector2f (position) of a specific base
+     * @param factions What factions are allowed in a specific base
+     * @param neighbouringCities Which city's have a direct connection to this base
+     */
+	
 	public Base(Vector2f position, List<Faction> factions, List<City> neighbouringCities, T... pieces) {
 		super(position, factions, neighbouringCities);
 		addPieces(pieces);
 	}
 	
+    /**
+     * @returns what faction has acces to a specific base
+     */
+	
 	public Faction getFaction() {
 		return factions.get(0);
 	}
+	
+    /**
+     * @returns the amount of barbarians in a base
+     */
 	
 	public int getPieceCount() {
 		return pieces.size();
 	}
 	
+    /**
+     * @param T... pieces Ads a variable amount of pieces to base
+     */
+	
 	public void addPieces(T... pieces) {
 		this.pieces.addAll(Arrays.asList(pieces));
 	}
+	
+    /**
+     * @returns the piece that was removed
+     */
 	
 	public T removePiece() {
 		if (pieces.size() <= 0) return null;
 		return pieces.remove(0);
 	}
+	
+    /**
+     * @param pieceCount The specified amount of pieces that need to be removed from a base
+     * @returns a list with the pieces that where removed
+     */
 	
 	public List<T> removePieces(int piecesCount) {
 		List<T> removed = new ArrayList<T>();
