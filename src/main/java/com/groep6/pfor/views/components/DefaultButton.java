6819325d11531053418f6f47865331bb52b2b226
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-05-27 19:32:40 +0200

    Changed font and font size for default button

--- src/main/java/com/groep6/pfor/views/components/DefaultButton.java
+++ src/main/java/com/groep6/pfor/views/components/DefaultButton.java
@@ -9,6 +9,9 @@ import javafx.scene.layout.Background;
 import javafx.scene.layout.BackgroundFill;
 import javafx.scene.layout.CornerRadii;
 import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
 
 /**
  * @author Bastiaan Jansen
@@ -28,6 +31,7 @@ public class DefaultButton extends Button {
         setBackground(new Background(new BackgroundFill(Color.web("#db8937"), CornerRadii.EMPTY, Insets.EMPTY)));
         setPadding(new Insets(10, 10, 10, 10));
         setStyle("-fx-text-fill: white");
+        setFont(Font.font("verdana", FontWeight.NORMAL, FontPosture.REGULAR, 15));
 
         addEventHandler(MouseEvent.MOUSE_ENTERED, new EventHandler<MouseEvent>() {
             @Override public void handle(MouseEvent e) {
