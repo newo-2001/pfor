de04e7497fa40e3102eed4d184099aad2ab9a6bd
Owen Elderbroek <s1122238@student.hsleiden.nl>
2020-05-27 14:06:20 +0200

    Refactored Base

--- src/main/java/com/groep6/pfor/models/Base.java
+++ src/main/java/com/groep6/pfor/models/Base.java
@@ -1,42 +1,37 @@
 package com.groep6.pfor.models;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 public class Base<T extends Piece> extends Tile {
-	
 	private List<T> pieces = new ArrayList<T>();
 	
-	public Base(ArrayList<T> pieces) {
-		this.pieces = pieces;
+	public Base(T... pieces) {
+		this.pieces = Arrays.asList(pieces);
 	}
 	
 	public Faction getFaction() {
-		Faction faction = factions.get(0);
-		return faction;
+		return factions.get(0);
 	}
 	
 	public int getPieceCount() {
 		return pieces.size();
 	}
 	
-	public void addPieces(ArrayList<T> pieces) {				//Dit klopt niet maar ik weet het even niet
-		this.pieces.addAll(pieces);	
+	public void addPieces(T... pieces) {
+		this.pieces.addAll(Arrays.asList(pieces));
 	}
 	
 	public T removePiece() {
-		T piece = pieces.get(0);
-		pieces.remove(0);
-		return piece;
+		if (pieces.size() <= 0) return null;
+		return pieces.remove(0);
 	}
 	
-	public List<T> removePieces(int pieces) {
-		while(this.pieces.size() > 0) {
-			for(int x = 0; x < pieces; x += 1) {
-				this.pieces.remove(0);
-		}
-			return this.pieces;
-		} 
-		return this.pieces;
+	public T[] removePieces(int piecesCount) {
+		piecesCount = Math.min(piecesCount, pieces.size());
+		List<T> removed = new ArrayList<T>();
+		for(int i = 0; i < piecesCount; i++) removed.add(this.pieces.remove(0));
+		return (T[]) removed.toArray();
 	}
 }
