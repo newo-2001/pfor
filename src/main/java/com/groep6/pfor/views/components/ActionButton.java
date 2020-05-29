c7166d9aa281a0719480c94aa77dd83779e8ef29
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-05-29 19:57:33 +0200

    Update ActionButton.

--- src/main/java/com/groep6/pfor/views/components/ActionButton.java
+++ src/main/java/com/groep6/pfor/views/components/ActionButton.java
@@ -1,5 +1,22 @@
 package com.groep6.pfor.views.components;
 
-public class ActionButton {
+import javafx.geometry.Insets;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.Border;
+import javafx.scene.layout.BorderStroke;
+import javafx.scene.layout.BorderStrokeStyle;
+import javafx.scene.layout.BorderWidths;
+import javafx.scene.layout.CornerRadii;
+import javafx.scene.paint.Color;
+
+public class ActionButton extends UIButton {
+	
+	public ActionButton(String text) {
+		super(text);
+		setPrefSize(150, 60);
+		setBackground(new Background(new BackgroundFill(Color.web("#ef4140"), CornerRadii.EMPTY, Insets.EMPTY)));
+		setBorder(new Border(new BorderStroke(Color.WHITE, BorderStrokeStyle.SOLID, CornerRadii.EMPTY, BorderWidths.DEFAULT)));
+	}
 
 }
