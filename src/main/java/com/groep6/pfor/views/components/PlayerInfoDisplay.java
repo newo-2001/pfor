f78631cd10cf5304765c9e4ad8437d40a7bde0d7
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-05-29 19:22:25 +0200

    Create and implement PlayerInfoDisplay (HBox with player info).

---
+++ src/main/java/com/groep6/pfor/views/components/PlayerInfoDisplay.java
@@ -0,0 +1,47 @@
+package com.groep6.pfor.views.components;
+
+import javafx.geometry.Insets;
+import javafx.scene.Node;
+import javafx.scene.layout.HBox;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.Circle;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontPosture;
+import javafx.scene.text.FontWeight;
+import javafx.scene.text.Text;
+
+public class PlayerInfoDisplay extends HBox {
+	
+	public PlayerInfoDisplay(Color playerColor, int playerNumber, String userName, String playerRole) {
+		createDisplay(playerColor, playerNumber, userName, playerRole);
+		setPadding(new Insets(0, 20, 0, 20));
+		for (Node child: this.getChildren()) {
+			setMargin(child, new Insets(0, 3, 0, 3));
+		}
+	}
+	
+	private void createDisplay(Color playerColor, int playerNumber, String userName, String playerRole) {
+		
+		Circle playerDot = new Circle();
+		playerDot.setFill(playerColor);
+		playerDot.setRadius(12);
+		
+		Text playerID = new Text("Speler " + playerNumber);
+		playerID.setFont(Font.font("verdana", FontWeight.NORMAL,
+                FontPosture.REGULAR, 13));
+        playerID.setFill(Color.WHITE);
+        
+        Text playerName = new Text(userName);
+        playerName.setFont(Font.font("verdana", FontWeight.NORMAL,
+        		FontPosture.REGULAR, 13));
+        playerName.setFill(Color.WHITE);
+        
+        Text role = new Text("(" + playerRole + ")");
+        role.setFont(Font.font("verdana", FontWeight.NORMAL,
+        		FontPosture.REGULAR, 13));
+        role.setFill(Color.WHITE);
+		
+		getChildren().addAll(playerDot, playerID, playerName, role);
+	}
+
+}
