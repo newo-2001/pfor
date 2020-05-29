e518528c6886cef9285f27f7229cfa658e99fc21
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-05-29 09:39:38 +0200

    Basis menuView

--- src/main/java/com/groep6/pfor/controllers/MenuController.java
+++ src/main/java/com/groep6/pfor/controllers/MenuController.java
@@ -2,9 +2,7 @@ package com.groep6.pfor.controllers;
 
 import com.groep6.pfor.models.Board;
 import com.groep6.pfor.util.IObserver;
-import com.groep6.pfor.views.BoardView;
-import com.groep6.pfor.views.MenuView;
-import com.groep6.pfor.views.View;
+import com.groep6.pfor.views.*;
 
 /**
  * @author Bastiaan Jansen
@@ -13,6 +11,14 @@ public class MenuController extends Controller {
 
     public void buttonClicked() {
         viewController.showView(new BoardView(viewController.getPrimaryStage()));
+    }
+
+    public void goToHostView() {
+        viewController.showView(new HostView(viewController.getPrimaryStage()));
+    }
+
+    public void goToJoinView() {
+        viewController.showView(new JoinView(viewController.getPrimaryStage()));
     }
 
     @Override
