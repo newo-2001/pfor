9892944864b885616373bc483f5aaf7994c5e094
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-05-29 09:30:21 +0200

    Add JavaDoc to DiceFace.

--- src/main/java/com/groep6/pfor/models/DiceFace.java
+++ src/main/java/com/groep6/pfor/models/DiceFace.java
@@ -1,5 +1,10 @@
 package com.groep6.pfor.models;
 
+/**
+ * Faces of the dice in Pandemic: Fall of Rome. BARBARIAN face is on the dice twice.
+ * @author Mitchell van Rijswijk
+ *
+ */
 public enum DiceFace {
 	BARBARIAN,
 	LEGION,
