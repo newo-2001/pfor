9dddb3ccd4a50786dcd1288cb5848f3bee2c26f0
nilse <nilse@DESKTOP-Q197SHS.domain_not_set.invalid>
2020-06-01 15:14:30 +0200

    Created TradeView and TradeController

--- src/main/java/com/groep6/pfor/controllers/TradeController.java
+++ src/main/java/com/groep6/pfor/controllers/TradeController.java
@@ -1,16 +1,35 @@
 package com.groep6.pfor.controllers;
 
+import java.util.List;
+
+import com.groep6.pfor.models.City;
+import com.groep6.pfor.models.Faction;
+import com.groep6.pfor.models.Game;
+import com.groep6.pfor.models.cards.Card;
+import com.groep6.pfor.models.cards.CityCard;
 import com.groep6.pfor.util.IObserver;
+import com.groep6.pfor.util.Vector2f;
 import com.groep6.pfor.views.TradeView;
 
 public class TradeController extends Controller {
+	
+	private Game game = Game.getInstance();
 
     public TradeController() {
-        viewController.showView(new TradeView(this));
+        Faction[] factions = new Faction[] {};
+    	game.getTradeDeck().addCards(new CityCard("Card 1", new City("City 1", false, new Vector2f(), factions), Faction.ANGLO_SAXSONS_FRANKS));
+    	game.getTradeDeck().addCards(new CityCard("Card 2", new City("City 1", false, new Vector2f(), factions), Faction.ANGLO_SAXSONS_FRANKS));
+    	game.getTradeDeck().addCards(new CityCard("Card 3", new City("City 1", false, new Vector2f(), factions), Faction.ANGLO_SAXSONS_FRANKS));
+    	game.getTradeDeck().addCards(new CityCard("Card 4", new City("City 1", false, new Vector2f(), factions), Faction.ANGLO_SAXSONS_FRANKS));
+    	
+    	viewController.showView(new TradeView(this));
+    }
+    
+    public List<Card> getTradeCard() {
+        return game.getTradeDeck().getCards();
     }
 
     @Override
     public void registerObserver(IObserver view) {
-
     }
 }
