7e6e44c8f7e2dcf754bbf8c7b9a1dc80782d2a72
nilse <nilse@DESKTOP-Q197SHS.domain_not_set.invalid>
2020-06-01 14:05:36 +0200

    Added javadoc to RoleCardInfoView

--- src/main/java/com/groep6/pfor/views/RoleCardInfoView.java
+++ src/main/java/com/groep6/pfor/views/RoleCardInfoView.java
@@ -21,12 +21,18 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 
 /**
- * The view that shows a card's information
+ * The view that shows the role card's information
  * @author Nils van der Velden
  */
 public class RoleCardInfoView extends View {
 
+    /** The RoleCardInfo controller  */
     private RoleCardInfoController roleCardInfoController;
+    
+    /**
+     * Initializes the RoleCardInfoView
+     * @param the roleCardInfoController
+     */
 
     public RoleCardInfoView(RoleCardInfoController roleCardInfoController) {
         this.roleCardInfoController = roleCardInfoController;
@@ -35,39 +41,52 @@ public class RoleCardInfoView extends View {
     }
 
     public void createView() {
+        /** Pane to put the different elements in  */
         BorderPane root = new BorderPane();
+        
+        /** ScrollPane to allow the user to scroll through the different role cards  */
         ScrollPane scrollPane = new ScrollPane();
         scrollPane.setFitToWidth(true);
         scrollPane.setFitToHeight(true);
+        
+        /** FlowPane that represents the individual cards */
         FlowPane cardsPane = new FlowPane();
         cardsPane.setPadding(new Insets(20, 20, 20, 20));
         cardsPane.setVgap(50);
         cardsPane.setHgap(50);
 
+        /** For loop to get the different role cards and put there information in the flowPane*/
         for (RoleCard card: roleCardInfoController.getRoleCards()) {
             UICard uicard = new UICard(card.getName());
             cardsPane.getChildren().add(uicard);
         }
-        
+        /** Put the different cards in the scrollPane */
         scrollPane.setContent(cardsPane);
         
-        VBox buttonsPane = new VBox(20);
-        buttonsPane.setAlignment(Pos.CENTER);
-        buttonsPane.setPadding(new Insets(50, 50, 50, 50));
+        /** ButtonPane to put the button in */
+        VBox buttonPane = new VBox(20);
+        buttonPane.setAlignment(Pos.CENTER);
+        buttonPane.setPadding(new Insets(50, 50, 50, 50));
         
+        /** Creates the back button */
         Button goBackButton = new UIButton("Ga terug");
         goBackButton.setPrefWidth(150);
         goBackButton.setBackground(new Background(new BackgroundFill(Color.web("#878787"), CornerRadii.EMPTY, Insets.EMPTY)));
         goBackButton.addEventFilter(MouseEvent.MOUSE_CLICKED, goBack);
 
-        buttonsPane.getChildren().addAll(goBackButton);
-        buttonsPane.setBackground(new Background(new BackgroundFill(Color.web("#D5544F"), CornerRadii.EMPTY, Insets.EMPTY)));
+        /** Puts the back button in the buttonBox */
+        buttonPane.getChildren().addAll(goBackButton);
+        buttonPane.setBackground(new Background(new BackgroundFill(Color.web("#D5544F"), CornerRadii.EMPTY, Insets.EMPTY)));
 
+        /** Puts the scrollPane and buttonPane into the BorderPane */
         root.setCenter(scrollPane);
-        root.setRight(buttonsPane);
+        root.setRight(buttonPane);
+        
+        /** Creates a scene that uses the BorderPane filled with elements */
         scene = new Scene(root);
     }
 
+    	/** Event handler for the back button */
         EventHandler<javafx.scene.input.MouseEvent> goBack = new EventHandler<javafx.scene.input.MouseEvent>() {
             @Override
             public void handle(javafx.scene.input.MouseEvent e) {
