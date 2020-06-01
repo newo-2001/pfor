e2bfe202b9078deff5f00d0c3e750259570c766d
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-06-01 15:13:34 +0200

    Add play functionality.

--- src/main/java/com/groep6/pfor/controllers/MediaController.java
+++ src/main/java/com/groep6/pfor/controllers/MediaController.java
@@ -2,8 +2,37 @@ package com.groep6.pfor.controllers;
 
 import com.groep6.pfor.util.IObserver;
 
-public class MediaController extends Controller {
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
+/**
+ * Media controller to play, loop and stop audio files.
+ * @author Mitchell van Rijswijk
+ *
+ */
+public class MediaController extends Controller {
+	
+	MediaPlayer mp;
+
+	/**
+	 * Plays a sound file.
+	 * 
+	 */
+	public void play(String filePath) {
+		Media sound = new Media("filePath");
+		mp = new MediaPlayer(sound);
+		mp.setVolume(0.3);
+		mp.setAutoPlay(true);
+	}
+	
+	/**
+	 * Stops currently playing sound file.
+	 * 
+	 */
+	public void stop() {
+		
+	}
+	
 	@Override
 	public void registerObserver(IObserver view) {}
 
