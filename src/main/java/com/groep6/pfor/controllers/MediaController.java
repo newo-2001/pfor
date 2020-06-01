4c2e5a3a7d15f3a64fd59c69b0e30ba4c7054a11
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-06-01 14:48:59 +0200

    Add MediaController.

---
+++ src/main/java/com/groep6/pfor/controllers/MediaController.java
@@ -0,0 +1,10 @@
+package com.groep6.pfor.controllers;
+
+import com.groep6.pfor.util.IObserver;
+
+public class MediaController extends Controller {
+
+	@Override
+	public void registerObserver(IObserver view) {}
+
+}
