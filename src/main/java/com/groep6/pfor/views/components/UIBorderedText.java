ae7b76293cc55e5940fbbb23a4c3f71ec8150a83
Mathijs <parrel@users.noreply.github.com>
2020-06-01 09:35:18 +0200

    Created component UIBorderedText

---
+++ src/main/java/com/groep6/pfor/views/components/UIBorderedText.java
@@ -0,0 +1,15 @@
+package com.groep6.pfor.views.components;
+
+import javafx.scene.text.Font;
+import javafx.scene.text.Text;
+
+public class UIBorderedText extends Text {
+    public UIBorderedText(String text, String borderColor, int borderWidth, String fillColor) {
+        super(text);
+        setStyle(borderColor, borderWidth, fillColor);
+    }
+
+    private void setStyle(String borderColor, int borderWidth, String fillColor) {
+        setStyle(String.format("-fx-stroke: %s; -fx-stroke-width: %2dpx; -fx-font-weight: bold; -fx-fill: %s;", borderColor, borderWidth, fillColor));
+    }
+}
