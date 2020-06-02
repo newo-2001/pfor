a98a69c1231aaf5ff34e0601114fed79a3616512
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-06-02 10:49:40 +0200

    Add JavaDoc

--- src/main/java/com/groep6/pfor/models/actions/IPlayerAction.java
+++ src/main/java/com/groep6/pfor/models/actions/IPlayerAction.java
@@ -1,7 +1,17 @@
 package com.groep6.pfor.models.actions;
 
+/**
+ * Interface for all actions a player can perform. These actions are the ones tied to the
+ * core game. These are not the card actions.
+ * @author Mitchell van Rijswijk.
+ *
+ */
 public interface IPlayerAction {
 
+	/**
+	 * Perform action.
+	 * 
+	 */
 	void execute();
 	
 }
