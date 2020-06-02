8b37adfed5a913e663213bfed76a9ff41ffbbe67
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-06-02 16:29:55 +0200

    Joining lobby functionality

--- src/main/java/com/groep6/pfor/controllers/JoinController.java
+++ src/main/java/com/groep6/pfor/controllers/JoinController.java
@@ -7,8 +7,11 @@ package com.groep6.pfor.controllers;
 
 import com.groep6.pfor.exceptions.EmptyFieldException;
 import com.groep6.pfor.exceptions.IncorrentPasswordException;
+import com.groep6.pfor.exceptions.NoDocumentException;
 import com.groep6.pfor.models.Game;
 import com.groep6.pfor.models.Lobby;
+import com.groep6.pfor.models.LobbyPlayer;
+import com.groep6.pfor.services.LobbyService;
 import com.groep6.pfor.util.IObserver;
 import com.groep6.pfor.views.JoinView;
 
@@ -23,15 +26,17 @@ public class JoinController extends Controller {
     public void joinLobby(String code, String username, String password) throws EmptyFieldException {
         if (username.isEmpty()) throw new EmptyFieldException("Username cannot be empty");
         else if(code.isEmpty()) throw new EmptyFieldException("Unique code cannot be empty");
-        
-        Lobby lobby = new Lobby(password);
+
         
         try {
-        	lobby.join(code, username, password, true);
+
+            LobbyService lobbyService = new LobbyService();
+            Lobby lobby = lobbyService.get(code);
+            LobbyPlayer lobbyPlayer = lobby.join(code, username, password, true);
+
+            new LobbyController(lobby);
         	
-        	new LobbyController(lobby);
-        	
-        } catch (IncorrentPasswordException error) {
+        } catch (IncorrentPasswordException | NoDocumentException error) {
             System.out.println("Error: " + error.getMessage());
         } 
     }
