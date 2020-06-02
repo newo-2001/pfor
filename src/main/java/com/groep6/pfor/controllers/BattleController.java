00d2c4dff0421d039f46b5908772ae5c92b30867
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-06-02 09:44:40 +0200

    Give BattleController a temp amount of barbarians and legions to fight.

--- src/main/java/com/groep6/pfor/controllers/BattleController.java
+++ src/main/java/com/groep6/pfor/controllers/BattleController.java
@@ -1,16 +1,40 @@
 package com.groep6.pfor.controllers;
 
+import com.groep6.pfor.models.Barbarian;
+import com.groep6.pfor.models.Faction;
+import com.groep6.pfor.models.Legion;
 import com.groep6.pfor.util.IObserver;
 import com.groep6.pfor.views.BattleView;
 
+/**
+ * Controller for the battle system. Gets information from the current city and performs a battle.
+ * Afterwards the results of the battle are shown in the BattleView.
+ * @author Mitchell van Rijswijk
+ *
+ */
 public class BattleController extends Controller {
 
-    public BattleController() {
-        viewController.showView(new BattleView(this));
-    }
+	// Array simulation of barbarians and legions in a city.
+	// TODO implement data aquisition from the current players current city.
+	private Barbarian[] barbarians = { new Barbarian(Faction.ANGLO_SAXSONS_FRANKS), new Barbarian(Faction.ANGLO_SAXSONS_FRANKS),
+			new Barbarian(Faction.ANGLO_SAXSONS_FRANKS) };
+	private Legion[] legions = {new Legion(), new Legion(), new Legion()};
 
-    @Override
-    public void registerObserver(IObserver view) {
+	/**
+	 * Constructor for BattleController. First performs a battle, then places the result in a new BattleView.
+	 * 
+	 */
+	public BattleController() {
+		battle();
+		viewController.showView(new BattleView(this));
+	}
 
-    }
+	public void battle() {
+
+	}
+
+	@Override
+	public void registerObserver(IObserver view) {
+
+	}
 }
