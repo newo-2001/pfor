f7901cafcd55bc7586fbeeea9af92b292e84f5e8
Mathijs <parrel@users.noreply.github.com>
2020-06-02 13:10:52 +0200

    Added instructionView

--- src/main/java/com/groep6/pfor/views/InstructionView.java
+++ src/main/java/com/groep6/pfor/views/InstructionView.java
@@ -1,6 +1,21 @@
 package com.groep6.pfor.views;
 
 import com.groep6.pfor.controllers.InstructionController;
+import com.groep6.pfor.views.components.UIButton;
+import com.sandec.mdfx.MDFXNode;
+import javafx.event.EventHandler;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.image.Image;
+import javafx.scene.layout.*;
+
+import javafx.scene.input.MouseEvent;
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
 /**
  * The view where the game's instructions are shown
@@ -9,6 +24,7 @@ import com.groep6.pfor.controllers.InstructionController;
 public class InstructionView extends View {
     /** The instructionController */
     private InstructionController instructionController;
+    private BorderPane root;
 
     /**
      * The constructor
@@ -16,5 +32,43 @@ public class InstructionView extends View {
      */
     public InstructionView(InstructionController instructionController) {
         this.instructionController = instructionController;
+
+        root = new BorderPane();
+
+        String markdown = "";
+        try {
+            markdown = new String(Files.readAllBytes(Paths.get(getClass().getResource("/misc/game_rules.md").toURI())));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+
+        MDFXNode mdfxNode = new MDFXNode(markdown);
+        mdfxNode.getStylesheets().add("/stylesheets/game_rules.css");
+        ScrollPane content = new ScrollPane(mdfxNode);
+        content.setFitToHeight(true);
+        content.setFitToWidth(true);
+
+        BackgroundImage backgroundImage = new BackgroundImage(new Image("images/paper_background.jpg"),
+                BackgroundRepeat.NO_REPEAT, BackgroundRepeat.NO_REPEAT,
+                BackgroundPosition.CENTER, new BackgroundSize(100, 100, true, true, true, true));
+        mdfxNode.setPadding(new Insets(50, 200, 50, 200));
+        mdfxNode.setBackground(new Background(backgroundImage));
+
+        Button goBackButton = new UIButton("Ga terug");
+        goBackButton.addEventFilter(MouseEvent.MOUSE_CLICKED, goBack);
+
+        HBox buttonPane = new HBox(12, goBackButton);
+        buttonPane.setAlignment(Pos.BOTTOM_RIGHT);
+        mdfxNode.getChildren().add(0, buttonPane);
+
+        root.setPrefWidth(300);
+        root.setCenter(content);
     }
+
+    EventHandler<MouseEvent> goBack = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+            instructionController.goBack();
+        }
+    };
 }
