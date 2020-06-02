a7f195949e47b6f3f8d72c46e0fb5993e0b7bb66
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-06-02 11:54:16 +0200

    Faction factory

--- src/main/java/com/groep6/pfor/factories/FactionFactory.java
+++ src/main/java/com/groep6/pfor/factories/FactionFactory.java
@@ -2,6 +2,7 @@ package com.groep6.pfor.factories;
 
 import com.groep6.pfor.models.factions.Faction;
 import com.groep6.pfor.models.factions.FactionType;
+import javafx.scene.paint.Color;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,7 +12,22 @@ public class FactionFactory {
     List<Faction> factions = new ArrayList<>();
 
     public FactionFactory() {
-//        factions.add(new Faction(FactionType.));
+        factions.add(new Faction(FactionType.ANGLO_SAXSONS_FRANKS, Color.ORANGE));
+        factions.add(new Faction(FactionType.VANDELS, Color.PURPLE));
+        factions.add(new Faction(FactionType.HUNS, Color.GREEN));
+        factions.add(new Faction(FactionType.VISIGOTHS, Color.WHITE));
+        factions.add(new Faction(FactionType.OSTROGOTHS, Color.BLUE));
     }
 
+    public List<Faction> getFactions() {
+        return factions;
+    }
+
+    public Faction getFaction(FactionType factionType) {
+        for (Faction faction: factions) {
+            if (faction.getFactionType() == factionType) return faction;
+        }
+
+        return null;
+    }
 }
