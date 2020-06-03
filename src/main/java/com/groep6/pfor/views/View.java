d23859f20209047f60c200fefa5e4187a0995906
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-06-03 11:09:11 +0200

    Start work on pillar- and letterboxing.

--- src/main/java/com/groep6/pfor/views/View.java
+++ src/main/java/com/groep6/pfor/views/View.java
@@ -1,10 +1,17 @@
 package com.groep6.pfor.views;
 
-import com.groep6.pfor.util.IObserver;
+import javafx.geometry.Rectangle2D;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
+import javafx.scene.layout.BackgroundImage;
+import javafx.scene.layout.BackgroundPosition;
+import javafx.scene.layout.BackgroundRepeat;
+import javafx.scene.layout.BackgroundSize;
+import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
+import javafx.stage.Screen;
 
 /**
  * @author Bastiaan Jansen
@@ -12,6 +19,24 @@ import javafx.stage.Stage;
 public abstract class View {
 
     protected Scene scene;
+    protected Rectangle2D viewport;
+    
+    public View() {
+    	viewport = Screen.getPrimary().getBounds();
+    	setBackground(getRoot(), Color.BLACK);
+    	checkPillarBoxing();
+    	checkLetterBoxing();
+    }
+    
+    protected void checkPillarBoxing() {
+    	if (getRoot().getHeight() > viewport.getHeight()) {
+    		
+    	}
+    }
+    
+    protected void checkLetterBoxing() {
+    	
+    }
 
     public abstract Pane getRoot();
 
@@ -21,4 +46,9 @@ public abstract class View {
                 BackgroundPosition.CENTER, new BackgroundSize(100, 100, true, true, true, true));
         layer.setBackground(new Background(backgroundImage));
     }
+    
+    protected void setBackground(Pane layer, Color color) {
+        layer.setBackground(new Background(new BackgroundFill(color, null, null)));
+    }
+    
 }
