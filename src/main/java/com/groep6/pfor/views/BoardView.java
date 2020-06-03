fdf950236087fdd1b8f2940a29071a412a4b0a17
nilse <nilse@DESKTOP-Q197SHS.domain_not_set.invalid>
2020-06-03 10:19:22 +0200

    Added trade to BoardView

--- src/main/java/com/groep6/pfor/views/BoardView.java
+++ src/main/java/com/groep6/pfor/views/BoardView.java
@@ -5,6 +5,7 @@ import com.groep6.pfor.controllers.BoardController;
 import com.groep6.pfor.controllers.HandController;
 import com.groep6.pfor.controllers.RecruitBarbarianController;
 import com.groep6.pfor.controllers.RecruitLegionController;
+import com.groep6.pfor.controllers.TradeController;
 import com.groep6.pfor.models.Player;
 import com.groep6.pfor.util.IObserver;
 import com.groep6.pfor.views.components.ActionButton;
@@ -83,6 +84,7 @@ public class BoardView extends View implements IObserver {
     EventHandler<MouseEvent> goToTradeView = new EventHandler<MouseEvent>() {
         @Override
         public void handle(MouseEvent e) {
+        	new TradeController();
             
         }
     };
