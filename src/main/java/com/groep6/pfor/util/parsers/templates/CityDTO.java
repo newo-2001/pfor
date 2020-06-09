ffdc6e9fdd5998b885d27fd1d768df15298bde01
Owen Elderbroek <s1122238@student.hsleiden.nl>
2020-06-09 11:23:08 +0200

    changed barbarians in cities to use bitfields, massively reducing data usage

--- src/main/java/com/groep6/pfor/util/parsers/templates/CityDTO.java
+++ src/main/java/com/groep6/pfor/util/parsers/templates/CityDTO.java
@@ -20,11 +20,11 @@ public class CityDTO extends DTO {
     public String name;
     public boolean fort;
     public int legions;
-    public Map<String, Integer> barbarians = new HashMap<>();
+    public int barbarians;
 
     public CityDTO() {}
 
-    private CityDTO(String name, boolean fort, int legions, Map<String, Integer> barbarians) {
+    private CityDTO(String name, boolean fort, int legions, int barbarians) {
         this.name = name;
         this.fort = fort;
         this.legions = legions;
@@ -36,16 +36,28 @@ public class CityDTO extends DTO {
         for (Faction faction : FactionFactory.getInstance().getFactions()) barbarians.put(faction.getFactionType().toString(), 0);
         for (Barbarian barbarian : city.getBarbarians())
             barbarians.put(barbarian.getFactionType().toString(), barbarians.get(barbarian.getFactionType().toString()) + 1);
-        return new CityDTO(city.getName(), city.hasFort(), city.getLegionCount(), barbarians);
+
+        int barbs = barbarians.get(FactionType.VISIGOTHS.toString());
+        barbs |= barbarians.get(FactionType.VANDALS.toString()) * 64;
+        barbs |= (int) (barbarians.get(FactionType.ANGLO_SAXSONS_FRANKS.toString()) * Math.pow(64, 2));
+        barbs |= (int) (barbarians.get(FactionType.HUNS.toString()) * Math.pow(64, 3));
+        barbs |= (int) (barbarians.get(FactionType.OSTROGOTHS.toString()) * Math.pow(64, 4));
+        return new CityDTO(city.getName(), city.hasFort(), city.getLegionCount(), barbs);
     }
 
     public City toModel() {
         Stack<Legion> legions = new Stack<>();
         for (int i = 0; i < this.legions; i++) legions.push(new Legion());
         Stack<Barbarian> barbarians = new Stack<>();
-        for (String faction : this.barbarians.keySet())
-            for (int i = 0; i < this.barbarians.get(faction); i++)
-                barbarians.push(new Barbarian(FactionType.valueOf(faction)));
+        addBarbarians(FactionType.VISIGOTHS, this.barbarians & 63, barbarians);
+        addBarbarians(FactionType.VANDALS, this.barbarians & (63 << 6) >> 6, barbarians);
+        addBarbarians(FactionType.ANGLO_SAXSONS_FRANKS, this.barbarians & (63 << 12) >> 12, barbarians);
+        addBarbarians(FactionType.HUNS, this.barbarians & (63 << 18) >> 18, barbarians);
+        addBarbarians(FactionType.OSTROGOTHS, this.barbarians & (63 << 24) >> 24, barbarians);
         return new City(name, legions, barbarians, fort);
+    }
+
+    private void addBarbarians(FactionType faction, int count, Stack<Barbarian> stack) {
+        for (int i = 0; i < count; i++) stack.push(new Barbarian(faction));
     }
 }
