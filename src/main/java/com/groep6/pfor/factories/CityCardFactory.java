e1389cec5465fe840e8b704cff40cca9e89ea518
nilse <nilse@DESKTOP-Q197SHS.domain_not_set.invalid>
2020-06-09 08:46:18 +0200

    Removed unused imports from CityCardFactory

--- src/main/java/com/groep6/pfor/factories/CityCardFactory.java
+++ src/main/java/com/groep6/pfor/factories/CityCardFactory.java
@@ -1,17 +1,8 @@
 package com.groep6.pfor.factories;
 
-import com.groep6.pfor.models.City;
 import com.groep6.pfor.models.Deck;
 import com.groep6.pfor.models.cards.CityCard;
-import com.groep6.pfor.models.cards.EventCard;
-import com.groep6.pfor.models.cards.actions.eventActions.DoUtDesAction;
-import com.groep6.pfor.models.cards.actions.eventActions.FaberFortunaeAction;
-import com.groep6.pfor.models.cards.actions.eventActions.HicManebimusOptimeAction;
-import com.groep6.pfor.models.cards.actions.eventActions.MortuiNonMordentAction;
-import com.groep6.pfor.models.cards.actions.eventActions.VitaMeaAction;
-import com.groep6.pfor.models.factions.Faction;
 import com.groep6.pfor.models.factions.FactionType;
-import com.groep6.pfor.util.Vector2f;
 
 public class CityCardFactory {
 	
