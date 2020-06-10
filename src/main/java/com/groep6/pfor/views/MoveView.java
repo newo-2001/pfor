5d8e8f35c7dd260f5fdfc3d0f74895a9e8b1ef4a
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-06-10 11:48:09 +0200

    New screen design

--- src/main/java/com/groep6/pfor/views/MoveView.java
+++ src/main/java/com/groep6/pfor/views/MoveView.java
@@ -1,112 +1,114 @@
 package com.groep6.pfor.views;
 
 import com.groep6.pfor.controllers.MoveController;
+import com.groep6.pfor.controllers.RecruitBarbarianController;
+
+/**
+ * The view where you can choose the amout of barbarians you want to recruit
+ * @author Nils van der Velden
+ */
+
 import com.groep6.pfor.util.IObserver;
 import com.groep6.pfor.views.components.UIButton;
 
+import com.groep6.pfor.views.components.UIText;
 import javafx.event.EventHandler;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Pane;
+import javafx.scene.layout.*;
 import javafx.scene.paint.Color;
-import javafx.scene.text.Font;
-import javafx.scene.text.FontPosture;
 import javafx.scene.text.FontWeight;
-import javafx.scene.text.Text;
 
-/**
- * The move view
- * @author Nils van der Velden
- */
 public class MoveView extends View implements IObserver {
+    /** The recruitLegionController */
     private MoveController moveController;
 
-    private BorderPane root;
-    
-    /**
-     * The constructor
-     * @param moveController The moveController
-     */
-    
+    private StackPane root;
+
     public MoveView(MoveController moveController) {
         this.moveController = moveController;
-    	moveController.registerObserver(this);
-    	
+        moveController.registerObserver(this);
+
         createView();
         update();
     }
-    
+
+
+
     public void createView() {
-    	
-        root = new BorderPane();
-    	
-    	Text text = new Text("Hoeveel legioenen wil je meenemen?");
-    	
-    	text.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR,20));
-    	text.setFill(Color.BLACK);
-    	root.setCenter(text);
-    	
+        root = new StackPane();
+
+        VBox box = new VBox();
+        box.setMaxSize(500, 300);
+        box.setAlignment(Pos.CENTER);
+        box.setSpacing(40);
+        box.setPadding(new Insets(50));
+        box.setBackground(new Background(new BackgroundFill(Color.web("D5544F"), CornerRadii.EMPTY, Insets.EMPTY)));
+
+        UIText text = new UIText("Hoeveel legioenen wil je meenemen?");
+        text.setSize(20).setWeight(FontWeight.BOLD).setColor(Color.WHITE);
+
         Button backButton = new UIButton("Go back");
         backButton.addEventFilter(MouseEvent.MOUSE_CLICKED, menuButtonClicked);
-        root.setTop(backButton);
-        
+        backButton.setBackground(new Background(new BackgroundFill(Color.web("#878787"), CornerRadii.EMPTY, Insets.EMPTY)));
+
         HBox buttonBox = new HBox(30);
         buttonBox.setAlignment(Pos.CENTER);
-        
-        Button takeOne = new UIButton("1");
-        takeOne.addEventFilter(MouseEvent.MOUSE_CLICKED, takeOneClicked);
-        
-        Button TakeTwo = new UIButton("2");
-        TakeTwo.addEventFilter(MouseEvent.MOUSE_CLICKED, takeTwoClicked);
-        
-        Button TakeThree = new UIButton("3");
-        TakeThree.addEventFilter(MouseEvent.MOUSE_CLICKED, takeThreeClicked);
-        
-        buttonBox.getChildren().addAll(takeOne, TakeTwo, TakeThree);
-        
-        BorderPane.setMargin(buttonBox, new Insets(12,12,100,12)); // optional
-        root.setBottom(buttonBox);
-}
-    
+
+        Button recruitOne = new UIButton("1");
+        recruitOne.addEventFilter(MouseEvent.MOUSE_CLICKED, recruitOneClicked);
+
+        Button recruitTwo = new UIButton("2");
+        recruitTwo.addEventFilter(MouseEvent.MOUSE_CLICKED, recruitTwoClicked);
+
+        Button recruitThree = new UIButton("3");
+        recruitThree.addEventFilter(MouseEvent.MOUSE_CLICKED, recruitThreeClicked);
+
+        buttonBox.getChildren().addAll(recruitOne, recruitTwo, recruitThree);
+
+        box.getChildren().addAll(text, buttonBox, backButton);
+
+        root.getChildren().add(box);
+
+        setBackground(root, "images/background-2.jpg");
+    }
+
     EventHandler<MouseEvent> menuButtonClicked = new EventHandler<MouseEvent>() {
         @Override
         public void handle(MouseEvent e) {
             moveController.goBack();
         }
     };
-    
-    EventHandler<MouseEvent> takeOneClicked = new EventHandler<MouseEvent>() {
+
+    EventHandler<MouseEvent> recruitOneClicked = new EventHandler<MouseEvent>() {
         @Override
         public void handle(MouseEvent e) {
-        	moveController.moveLegions(1);
-        }
-    };
-    
-    EventHandler<MouseEvent> takeTwoClicked = new EventHandler<MouseEvent>() {
-        @Override
-        public void handle(MouseEvent e) {
-        	moveController.moveLegions(2);
-        }
-    };
-    
-    EventHandler<MouseEvent> takeThreeClicked = new EventHandler<MouseEvent>() {
-        @Override
-        public void handle(MouseEvent e) {
-        	moveController.moveLegions(3);
+            moveController.moveLegions(1);
         }
     };
 
+    EventHandler<MouseEvent> recruitTwoClicked = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+            moveController.moveLegions(2);
+        }
+    };
 
+    EventHandler<MouseEvent> recruitThreeClicked = new EventHandler<MouseEvent>() {
+        @Override
+        public void handle(MouseEvent e) {
+            moveController.moveLegions(3);
+        }
+    };
 
     @Override
     public void update() {
+        // TODO Auto-generated method stub
 
     }
+
 
     @Override
     public Pane getRoot() {
