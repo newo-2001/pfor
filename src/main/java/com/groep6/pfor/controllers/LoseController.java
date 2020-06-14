e29a632684b041cd5782a8a5df97c92b2de329f9
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-06-14 12:21:35 +0200

    Add loss theme

--- src/main/java/com/groep6/pfor/controllers/LoseController.java
+++ src/main/java/com/groep6/pfor/controllers/LoseController.java
@@ -1,16 +1,23 @@
 package com.groep6.pfor.controllers;
 
+import com.groep6.pfor.Main;
 import com.groep6.pfor.util.IObserver;
 import com.groep6.pfor.views.LoseView;
 
 public class LoseController extends Controller {
 
     public LoseController() {
+    	changeMusic();
         viewController.showView(new LoseView(this));
     }
 
     public void goToMenuView() {
         new MenuController();
+    }
+    
+    public void changeMusic() {
+    	Main.musicManager.stop();
+    	Main.musicManager.play("src/main/resources/sounds/music/The_End_of_an_Era.mp3", 0.2, false);
     }
 
     @Override
