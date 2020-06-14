b4d5816a789a89e5b636ee6a8717c3d539f97d88
Mitchell van Rijswijk <s1121792@student.hsleiden.nl>
2020-06-14 13:55:14 +0200

    Add Enum GameState (MENU, GAME)

---
+++ src/main/java/com/groep6/pfor/models/GameState.java
@@ -0,0 +1,13 @@
+package com.groep6.pfor.models;
+
+/**
+ * Game state for a collection of screens. MENU for MenuView, HostView, JoinView, LobbyView.
+ * GAME for BoardView, MoveView, RecruitLegionView etc. Switch between states when switching between
+ * collections of views.
+ * @author Mitchell van Rijswijk
+ *
+ */
+public enum GameState {
+	MENU,
+	GAME;
+}
