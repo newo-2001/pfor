a8acd13c9fb3573f710640678a92745f4ee68808
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-06-15 13:09:30 +0200

    Updated my unit tests from JUnit 3 to JUnit 5

--- test/com/groep6/pfor/models/DeckTest.java
+++ test/com/groep6/pfor/models/DeckTest.java
@@ -2,53 +2,81 @@ package com.groep6.pfor.models;
 
 import com.groep6.pfor.factories.CityCardFactory;
 import com.groep6.pfor.models.cards.Card;
-import com.groep6.pfor.models.cards.CityCard;
-import junit.framework.TestCase;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 /**
  * @author Bastiaan Jansen
  */
-public class DeckTest extends TestCase {
+class DeckTest {
 
     private Deck deck;
     private Card card1;
     private Card card2;
 
-    public void setUp() throws Exception {
-        super.setUp();
-        this.deck = new Deck();
+    @BeforeEach
+    void setUp() {
         CityCardFactory cityCardFactory = CityCardFactory.getInstance();
         List<Card> cards = cityCardFactory.getCityCardDeck().getCards();
+        this.deck = new Deck();
         this.card1 = cards.get(0);
         this.card2 = cards.get(1);
     }
 
-    public void tearDown() throws Exception {
+    @AfterEach
+    void tearDown() {
         this.deck = null;
         this.card1 = null;
         this.card2 = null;
     }
 
-    public void testAddCards() {
+    @Test
+    void addCards() {
         deck.addCards(card1);
         assertEquals(1, deck.getCardCount());
     }
 
-    public void testMerge() {
+    @Test
+    void getCards() {
+        deck.addCards(card1, card2);
+        List<Card> cards = new ArrayList<>();
+        cards.add(card1);
+        cards.add(card2);
+        assertEquals(2, deck.getCardCount());
+        assertEquals(cards, deck.getCards());
+    }
+
+    @Test
+    void removeCard() {
+        deck.addCards(card1, card2);
+        deck.removeCard(card1);
+        assertEquals(1, deck.getCardCount());
+        assertEquals(card2, deck.draw());
+    }
+
+    @Test
+    void merge() {
         deck.addCards(card1);
         Deck deck2 = new Deck(card2);
         deck.merge(deck2);
         assertEquals(2, deck.getCardCount());
     }
 
-    public void testDraw() {
+    @Test
+    void draw() {
         deck.addCards(card1, card2);
         Card card = deck.draw();
         assertEquals(card2, card);
     }
 
-    public void testGetCardCount() {
+    @Test
+    void getCardCount() {
         deck.addCards(card1, card2);
         assertEquals(2, deck.getCardCount());
     }
