ef6447df13cef276087c728217ee73c4181aa140
Bastiaan Jansen <Bastiaanj7@outlook.com>
2020-06-15 12:36:19 +0200

    Test case hand

---
+++ test/com/groep6/pfor/models/HandTest.java
@@ -0,0 +1,16 @@
+package com.groep6.pfor.models;
+
+import junit.framework.TestCase;
+
+public class HandTest extends TestCase {
+
+    public void setUp() throws Exception {
+        super.setUp();
+    }
+
+    public void tearDown() throws Exception {
+    }
+
+    public void testRemoveCards() {
+    }
+}
